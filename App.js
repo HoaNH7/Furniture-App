d0725d754878bd6dd3314d7659ed3fe3e828f193
Hoa <hoanh472001@gmail.com>
2024-05-10 15:50:52 +0700

    add code

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from 'react';
 
 export default function App() {
+
+const [fontsLoaded] = useFonts({
+  regular: require(""),
+  bold: require("./assets/fonts"),
+  // extrabold: require("./assets/fonts/Poppins-ExtraBold"),
+  // light: require("./assets/fonts/Poppins-Light"),
+  // medium: require("./assets/fonts/Poppins-Medium"),
+  // semibold: require("./assets/fonts/Poppins-SemiBold"),
+})
+
+// const onLayoutRootView = useCallback(async() => {
+
+//   if(fontsLoaded){
+//     await SplashScreen.hideAsync();
+//   }
+// }, [fontsLoaded]);
+
+// if(!fontsLoaded){
+//   return null;
+// }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -17,4 +41,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textStyle: {
+    fontFamily: "extrabold",
+    fontSize: 20,
+  }
 });
